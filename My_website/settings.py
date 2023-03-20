96d8d5a88dc05b13adef1b84bfdd427339037689
sedate_0521 <sedate_0521@qq.com>
2023-03-20 10:03:47 +0800

    update configuration

--- My_website/settings.py
+++ My_website/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "my_website",
         "USER": "root",
-        "PASSWORD": "zh170305",
+        "PASSWORD": "1234",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
